7aa3957c805baf1e73f5ade5c328a3eb56363c58
Amanda Goforth <amandagoforth7@gmail.com>
2016-06-26 16:57:47 -0400

    Asking for help please

---
+++ src/js/main.js
@@ -0,0 +1,91 @@
+// 1. Write a function that utilizes recursion to calculate the factorial of a number.
+
+function factorial (x) {
+	if (x === 1) {
+		return 1;
+	} else {
+		return x * factorial(x - 1);
+	}
+}
+
+console.assert(factorial(5) === 120);
+console.assert(factorial(12) === 479001600);
+
+
+// 2.1 Write a function called walkTheDOM that will execute a callback function for every descendant of a node.
+
+function walkTheDOM (parent, callback) {
+	callback(parent);
+	for (var i = 0; i < parent.children.length; i++) {
+		walkTheDOM(parent.children[i], callback);
+	}
+}
+
+
+// 2.2 Write a function called findElementById that uses walkTheDOM to return the first element with the specified id in the document.
+
+// The function should take an id (string) as the first argument.
+// The function should return the first element with the specified id that is found.
+
+function findElementById (parent, id) {
+	var result;
+	walkTheDOM(parent, function (x) {
+		if (x.id === id && !result) {
+			result = x;
+		}
+	})
+	return result;
+}
+
+console.assert(document.getElementById('lookyHere') === findElementById(document.body, 'lookyHere'));
+console.assert(document.getElementById('bigWords') === findElementById(document.body, 'bigWords'));
+
+
+// 2.3 Write a function called findElementsByTagName that uses walkTheDOM to return an array of a node's descendants that have a specific tag name.
+
+// The function should take a starting node as the first argument followed by a tag name (e.g. "p", "nav").
+
+function findElementsByTagName (start, tagName) {
+	var result = [];
+	tagName = tagName.toUpperCase();
+	walkTheDOM(start, function (x) {
+		if (x.tagName === tagName) {
+			result.push(x);
+		}
+	})
+	return result;
+};
+
+console.assert(document.getElementsByTagName('main')[0] === findElementsByTagName(document.body, 'main')[0]);
+console.assert(document.getElementsByTagName('div')[1] === findElementsByTagName(document.body, 'div')[1]);
+
+
+// 2.4 Write a function called findElementsByClassName that uses walkTheDOM to return an array of a node's descendants that have a set of specific class names.
+
+// The function should take a starting node as the first argument followed by a variable number of class names (strings).
+// The function should return an array of elements.
+// The elements in the resulting array should have all of the class names that were specified, not just one.
+
+function findElementsByClassName (start, arguments) {
+	var result = [];
+	walkTheDOM(start, function (x) {
+		for (var i = 0; i < arguments.length; i++) {
+			if (x === x) {
+				result.push(x);
+			}
+		}
+	})
+	return result;
+};
+
+var please = findElementsByClassName(document.body, 'thisOne', 'thisOtherOne');
+console.log(please);
+
+console.log(document.body.classList.length);
+
+// var elementClasses = document.body.classList;
+// console.log(elementClasses);
+
+// console.assert(document.getElementsByClassName('thisOne')[0] === findElementsByClassName(document.body, 'thisOne', 'thisOtherOne')[0]);
+
+// console.log(document.getElementsByClassName('thisOne'));
